56295beafb5d58a763a9b249ab3d66e17548df5e
sergeyrid <sergeyrid@yandex.ru>
2022-06-04 01:55:47 +0300

    Fix

--- main.py
+++ main.py
@@ -7,27 +7,37 @@ from solver import check_sat, create_solver, get_final_values, Solver
 
 
 def add_constraints(file_name: str, s: Solver, info: str) -> Solver:
-    if not exists(file_name):
-        raise FileNotFoundError()
-    with open(file_name) as file:
-        text = file.readlines()
+    if exists(file_name):
+        with open(file_name) as file:
+            text = file.readlines()
+    else:
+        text = ''
     for line in text:
-        (var, op, const) = line.split()
+        (var1, op, var2) = line.split()
+        if var1.isdigit() or (len(var1) >= 2 and var1[0] == '-' and var1[1:].isdigit()):
+            var1 = int(var1)
+        else:
+            var1 = Int(var1 + info)
+        if var2.isdigit() or (len(var2) >= 2 and var2[0] == '-' and var2[1:].isdigit()):
+            var2 = int(var2)
+        else:
+            var2 = Int(var2 + info)
         if op == '==':
-            s.add(Int(var + info) == int(const))
+            s.add(var1 == var2)
         elif op == '>':
-            s.add(Int(var + info) > int(const))
+            s.add(var1 > var2)
         elif op == '<':
-            s.add(Int(var + info) < int(const))
+            s.add(var1 < var2)
     return s
 
 
 def solve_file(mode: str, prog_file_name: str, input_file_name: str,
                output_file_name: str) -> None:
-    if not exists(prog_file_name):
-        raise FileNotFoundError()
-    with open(prog_file_name) as file:
-        text = file.read()
+    if exists(prog_file_name):
+        with open(prog_file_name) as file:
+            text = file.read()
+    else:
+        text = ''
     p = Parser(text)
     commands, variables = p.get_parsed()
     s = create_solver(commands, variables)
@@ -59,3 +69,4 @@ def main() -> None:
 
 if __name__ == '__main__':
     main()
+
